5ea0609e9e286c4c1b700c1ea031e7412d8fa4eb
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-28 12:00:45 -0400

    commit 2

--- src/main/java/com/pluralsight/Manager/House.java
+++ src/main/java/com/pluralsight/Manager/House.java
@@ -1,4 +1,75 @@
 package com.pluralsight.Manager;
 
-public class House {
+public class House extends Asset{
+   private String address;
+    private int condition;
+    private int  squareFoot;
+    private int lotSize;
+
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+@Override
+    public double getValue() {
+    double pricePerSqFoot;
+        switch (condition){
+            case 1:
+                pricePerSqFoot = 180.00;
+                break;
+            case 2:
+                pricePerSqFoot = 130.00;
+                break;
+            case 3:
+                pricePerSqFoot = 90.00;
+                break;
+            case 4:
+                pricePerSqFoot = 80.00;
+                break;
+            default:
+                pricePerSqFoot = 0.0;
+        }
+
+    double baseValue = squareFoot * pricePerSqFoot;
+
+    double lotValue = lotSize * 0.25;
+
+    return baseValue + lotValue;
+}
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
 }
--- src/main/java/com/pluralsight/Manager/Vehicle.java
+++ src/main/java/com/pluralsight/Manager/Vehicle.java
@@ -1,4 +1,61 @@
 package com.pluralsight.Manager;
 
-public class Vehicle {
+public class Vehicle extends Asset {
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    @Override
+    public double getValue() {
+        double costPerYear = 0;
+        if (year <= 3) {
+            costPerYear = -0.03;
+        } else if (year == 4 && year <= 6) {
+            costPerYear = -0.06;
+        } else if (year == 7 && year <= 10) {
+            costPerYear = -0.08;
+        } else if (year > 10) {
+            costPerYear = -1000.00;
+        }
+        boolean isHondaOrToyota = makeModel.toLowerCase().contains("honda") ||
+                makeModel.toLowerCase().contains("toyota");
+
+        if (odometer > 100000 && !isHondaOrToyota) {
+            costPerYear = -0.25;
+        }
+double currentCost = originalCost -costPerYear;
+
+        return currentCost;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
 }
