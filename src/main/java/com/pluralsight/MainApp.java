43e744579daa7610b7ea21a61fec8092b02ebd3e
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-28 14:53:23 -0400

    commit 3

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -11,27 +11,38 @@ import java.util.ArrayList;
 public class MainApp {
     public static void main(String[] args) {
         ArrayList<Asset> asset = new ArrayList<>();
-        House house1 = new House("My house","Nov 2021",100000,"My address",1,24760,30000);
-asset.add(house1);
-        House house2 = new House("My vacation house","Nov 2024",800000,"My vacation address",1,204760,400000);
+        House house1 = new House("My house", "Nov 2021", 100000, "My address", 1, 24760, 30000);
+        asset.add(house1);
+        House house2 = new House("My vacation house", "Nov 2024", 800000, "My vacation address", 1, 204760, 400000);
         asset.add(house2);
 
-        Vehicle car1 = new Vehicle("Bruce's truck","Dec 2020", 20000,"Mercedes Bentz", 2020,10);
+        Vehicle car1 = new Vehicle("Bruce's truck", "Dec 2020", 20000, "Mercedes Bentz", 2020, 10);
         asset.add(car1);
-        Vehicle car2 = new Vehicle("Wayne's car","Dec 2025", 300000,"Bugatti Chiron", 2025,1);
+        Vehicle car2 = new Vehicle("Wayne's car", "Dec 2025", 300000, "Bugatti Chiron", 2025, 1);
         asset.add(car2);
 
-    }
-    public void assets(){
-        Asset asset = null;
         try {
-            BufferedReader bufreader = new BufferedReader(
-                    new FileReader("src/main/resources/Assets.csv"));
+            for (Asset a : asset) {
+                System.out.println("Description: " + a.getDescription());
+                System.out.println("Date Acquired: " + a.getDateAcquired());
+                System.out.println("Original Cost: $" + a.getOriginalCost());
+                System.out.println("Current Value: $" + a.getValue());
+                System.out.println();
 
-            asset.getDateAcquired();
+                // Check type
+                if (a instanceof House) {
+                    House h = (House) a;  // downcast
+                    System.out.println("Address: " + h.getAddress());
+                    System.out.println();
+                } else if (a instanceof Vehicle) {
+                    Vehicle c = (Vehicle) a;  // downcast
+                    System.out.println("Year/MakeModel: " + c.getYear() + " " + c.getMakeModel());
+                    System.out.println();
+                }
+            }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+                throw new RuntimeException(e);
         }
-
     }
+
 }
--- src/main/java/com/pluralsight/Manager/Asset.java
+++ src/main/java/com/pluralsight/Manager/Asset.java
@@ -46,4 +46,5 @@ public class Asset {
                 ", dateAcquired='" + dateAcquired + '\'' +
                 ", originalCost=" + originalCost;
     }
+
 }
